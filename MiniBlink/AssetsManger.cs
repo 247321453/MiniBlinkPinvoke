5acc4a6752164c7743690040216bd0d85b1e65eb
kenan <kn@aluohe.com>
2018-12-15 09:50:21 +0800

    protected

--- MiniBlink/AssetsManger.cs
+++ MiniBlink/AssetsManger.cs
@@ -9,20 +9,12 @@ namespace MiniBlinkPinvoke
 {
     public class AssetsManger
     {
-        private Assembly Assemblys;
-        private string mResNameSpace;
-        public AssetsManger() {
-            this.Assemblys = Assembly.GetExecutingAssembly();
-            this.mResNameSpace = "MiniBlinkPinvoke";
-        }
-
-        public AssetsManger(Assembly Assemblys, string _namespace)
+        public AssetsManger()
         {
-            this.Assemblys = Assemblys;
-            this.mResNameSpace = _namespace;
         }
 
-        public bool OnUrlLoad(string url, IntPtr job) {
+        public bool OnUrlLoad(string url, IntPtr job)
+        {
             if (url.StartsWith("mb://"))
             {
                 Regex regex = new Regex(@"mb://", RegexOptions.IgnoreCase | RegexOptions.Singleline | RegexOptions.IgnorePatternWhitespace);
@@ -39,66 +31,71 @@ namespace MiniBlinkPinvoke
             }
             return false;
         }
-        
-        protected virtual string PathToResName(string path) {
+
+        protected virtual string PathToResName(string path)
+        {
             return path.Replace('/', '.');
         }
 
-        private void LoadResource(string url, string path, IntPtr job)
+        protected virtual Stream Open(string path)
         {
             string rsName = PathToResName(path);
-            if (Assemblys != null)
-            {
-                using (Stream sm = Assemblys.GetManifestResourceStream(mResNameSpace + "." + rsName))
-                {
-                    if (sm != null)
-                    {
-                        StreamReader m_stream = new StreamReader(sm, Encoding.Default);
-                        m_stream.BaseStream.Seek(0, SeekOrigin.Begin);
-                        string strLine = m_stream.ReadToEnd();
-                        m_stream.Close();
-                        string data = strLine;
-                        if (url.EndsWith(".css"))
-                        {
-                            BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("text/css"));
-                        }
-                        else if (url.EndsWith(".png"))
-                        {
-                            BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("image/png"));
-                        }
-                        else if (url.EndsWith(".gif"))
-                        {
-                            BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("image/gif"));
-                        }
-                        else if (url.EndsWith(".jpg"))
-                        {
-                            BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("image/jpg"));
-                        }
-                        else if (url.EndsWith(".js"))
-                        {
-                            BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("application/javascript"));
-                        }
-                        else
-                        {
-                            BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("text/html"));
-                        }
-                        //wkeNetSetURL(job, url);
-                        BlinkBrowserPInvoke.wkeNetSetData(job, Marshal.StringToCoTaskMemAnsi(data), Encoding.Default.GetBytes(data).Length);
-                    }
-                    else
-                    {
-                        ResNotFond(url, job);
-                    }
-                }
-            }
-            else
+            Assembly Assemblys = Assembly.GetExecutingAssembly();
+            return Assemblys.GetManifestResourceStream("MiniBlinkPinvoke." + rsName);
+        }
+
+        protected void LoadResource(string url, string path, IntPtr job)
+        {
+            string rsName = PathToResName(path);
+            Stream sm = Open(path);
+            if (sm == null)
             {
                 ResNotFond(url, job);
+                return;
+            }
+            try
+            {
+                StreamReader m_stream = new StreamReader(sm, Encoding.Default);
+                m_stream.BaseStream.Seek(0, SeekOrigin.Begin);
+                string strLine = m_stream.ReadToEnd();
+                m_stream.Close();
+                string data = strLine;
+                if (url.EndsWith(".css"))
+                {
+                    BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("text/css"));
+                }
+                else if (url.EndsWith(".png"))
+                {
+                    BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("image/png"));
+                }
+                else if (url.EndsWith(".gif"))
+                {
+                    BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("image/gif"));
+                }
+                else if (url.EndsWith(".jpg"))
+                {
+                    BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("image/jpg"));
+                }
+                else if (url.EndsWith(".js"))
+                {
+                    BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("application/javascript"));
+                }
+                else
+                {
+                    BlinkBrowserPInvoke.wkeNetSetMIMEType(job, Marshal.StringToCoTaskMemAnsi("text/html"));
+                }
+                //wkeNetSetURL(job, url);
+                BlinkBrowserPInvoke.wkeNetSetData(job, Marshal.StringToCoTaskMemAnsi(data), Encoding.Default.GetBytes(data).Length);
+            }
+            finally
+            {
+                sm.Close();
             }
         }
 
-        protected virtual string Make404(string url) {
-            return "<html><head><title>404没有找到资源</title></head><body>404没有找到资源"+ url + "</body></html>";
+        protected virtual string Make404(string url)
+        {
+            return "<html><head><title>404没有找到资源</title></head><body>404没有找到资源:<br/>" + url + "</body></html>";
         }
 
         private void ResNotFond(string url, IntPtr job)
